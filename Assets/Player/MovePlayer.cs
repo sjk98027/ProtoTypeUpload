0653055d81d6a0bb40052a58af948831aab5d9ff
sjk98027 <kakrkddusdn@naver.com>
2016-06-14 17:01:26 +0900

    0614
    0614

--- Assets/Player/MovePlayer.cs
+++ Assets/Player/MovePlayer.cs
@@ -1,47 +1,136 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine; 
+using System.Collections; 
 [RequireComponent(typeof(CharacterController))]
 
 public class MovePlayer: MonoBehaviour {
 
 
 	[Range(0f, 150f)]public float moveSpeed;
-	[Range(0f, 2000f)]public float jumpSpeed;
+	[Range(0f, 10000f)]public float jumpSpeed;
 	CharacterController player;
-	private float gravity = 5f;
-	Vector3 moveDirection;
-	private float accel = 5f;
+
+	Vector3 movePosition;
+
+	float y = 0.0f; // 씨발이거
+	const float y_base = 0.5f;      // character y base;, 씨빨 이거
+	float deltahigh=0;
+	float gravity = 13.0f;     // gravity feel
+	int direction = 0;       // 0:stop0, 1:jump, 2:down
+//	int oneTimeCurrentJumpCount = 0; // 1jump play
+//	int currentJumpCount = 0; // jump count
+	// 설정값
+	const float jump_speed = 0.2f;  // 
+	const float jump_accell = 0.01f; // 
+
+	public int oneTimeJumpCount = 1; // 1jump play count;
+	public int maximumJumpCount = 1; // maximum jump play count;
+	CharacterController Player;
+
+
 
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<CharacterController> ();
-//		y = y_base;
+				y = y_base;
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		moveDirection = new Vector3 (0f, 0f, Input.GetAxis ("Horizontal"));
-		moveDirection = transform.TransformDirection (moveDirection);
-		moveDirection.x *= moveSpeed;
+		
+		JumpProcess();
 
+		//movePosition = new Vector3 (0f, 0f, Input.GetAxis ("Horizontal"));
+//		moveDirection = transform.TransformDirection (moveDirection);
+//		moveDirection.x *= moveSpeed;
+		movePosition = gameObject.transform.position;
+		movePosition.y = y;
+		gameObject.transform.position = movePosition;
 		//moveDirection.y *= jumpSpeed;
-
+		if (Input.GetKey (KeyCode.C)) {
+			DoJump ();
 
 		//Input.GetAxis("Vertical")"y축 변수를 위해 내버려둠"
-		if (player.isGrounded) {
-			
-			if (Input.GetKey (KeyCode.C)) {
-				moveDirection.y = jumpSpeed;
+//		if (player.isGrounded) {
+//			}
+		}
 
-				//Debug.Log (moveDirection.y);
+//		moveDirection.y -= gravity*Time.deltaTime;
+
+		//player.Move (moveDirection * Time.deltaTime);
+
+		//player.Move (moveDirection * Time.deltaTime);
+		movePosition.y -=gravity;
+	}
+
+	void DoJump() // 점프키 누를때 1회만 호출
+	{
+
+		//		if(oneTimeCurrentJumpCount == oneTimeJumpCount) 
+		//			return; 
+		//
+		//		if(currentJumpCount >= maximumJumpCount) 
+		//			return; 
+		//
+		//
+		//		oneTimeCurrentJumpCount++; 
+		//		currentJumpCount++; 
+		direction = 1; 
+		gravity = jump_speed; 
+
+
+	} 
+
+	void JumpProcess()
+	{
+		switch (direction)
+		{
+		case 0: // 2단 점프시 처리
+			{
+				if (y > y_base)
+				{
+					if (y >= jump_accell)
+					{
+						y -= jump_accell;
+						y -= gravity;
+					}
+					else
+					{
+						y = y_base;
+					}
+				}
+				break;
+			}
+		case 1: // up
+			{
+				y += gravity;
+				if (gravity <= 0.0f)
+				{
+					direction = 2;
+				}
+				else
+				{
+					gravity -= jump_accell;
+				}
+				break;
+			}
+
+		case 2: // down
+			{
+				y -= gravity;
+				if (y > y_base)
+				{
+					gravity += jump_accell;
+				}
+				else
+				{
+					direction = 0;
+					y = y_base;
+				}
+				break;
 			}
 		}
 
-			moveDirection.y -= gravity;
+	}
 
-
-			player.Move (moveDirection * Time.deltaTime);
-
-		}
 }
---
+++ Assets/Player/test/MovePlayer1.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine; 
+using System.Collections; 
+
+using UnityEngine; 
+using System.Collections; 
+
+public class MovePlayer1: MonoBehaviour {
+
+
+
+
+	Transform player;
+	Vector3 movePosition;
+	float x ;
+	float x_max;
+
+	float y ; // 
+	float y_base;      // character y base;, 
+	//float deltahigh=0;
+	float gravity = 13.0f;     // gravity feel
+	int direction = 0;       // 0:stop0, 1:jump, 2:down
+
+	float Gety_base(){
+		return player.transform.position.y;
+	}
+
+	// 설정값
+	const float jump_speed = 0.2f;  // 
+	const float jump_accell = 0.01f; // 
+
+
+
+	public int oneTimeJumpCount = 1; // 1jump play count;
+	public int maximumJumpCount = 1; // maximum jump play count;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Transform> ();
+		y_base = Gety_base();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 moveposition = gameObject.transform.position;
+		moveposition.y = y;
+		JumpProcess();
+//		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal", 0f, 0f));
+//			transform.Translate(moveDirection * Time.deltaTime);
+//
+
+		gameObject.transform.position = moveposition;
+
+		if (Input.GetKey (KeyCode.C)) {
+			DoJump ();
+//			if(Input.GetKey(KeyCode.LeftArrow)
+//				{moveposition.x > }
+
+		}
+
+	}
+
+	void OnTriggerEnter(Collider coll){
+		y_base = Gety_base ();
+		if (coll.gameObject.tag == "field") {
+			gravity = 0;
+			//jump_speed = 0;
+		}
+	}
+
+
+
+	void DoMove()
+	{
+		
+	}
+	void DoJump() // 
+	{
+
+
+		direction = 1; 
+		gravity = jump_speed; 
+
+
+	} 
+
+	void JumpProcess()
+	{
+		switch (direction)
+		{
+		case 0: // 
+			{
+				if (y > y_base)
+				{
+					if (y >= jump_accell)
+					{
+						y -= jump_accell;
+						y -= gravity;
+					}
+					else
+					{
+						y = y_base;
+					}
+				}
+				break;
+			}
+		case 1: // up
+			{
+				y += gravity;
+				if (gravity <= 0.0f)
+				{
+					direction = 2;
+				}
+				else
+				{
+					gravity -= jump_accell;
+				}
+				break;
+			}
+
+		case 2: // down
+			{
+				y -= gravity;
+				if (y > y_base) {
+					gravity += jump_accell;
+				} 
+
+				else if (y <= y_base) {
+					gravity += jump_accell;
+
+				}	
+				
+				else
+				{
+					direction = 0;
+					y = y_base;
+				}
+
+				break;
+			}
+		}
+
+	}
+
+}
+
+
+
+
+
+
